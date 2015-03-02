021ea12b1ec48aff9db8daeba6ea79ec7a82a83a
Gariben <jacob.arlo.leibeck@gmail.com>
2015-03-01 22:28:04 -0500

    I saw that I could drop it down to twenty lines, so I did.

--- Monty_Hall/Racket/current/Monty_Hall.rkt
+++ Monty_Hall/Racket/current/Monty_Hall.rkt
@@ -1,20 +1,14 @@
 #lang racket
-
 (struct wins [stay switch] #:transparent)
-
 (define (random-door) 
   (+ 1 (random 3)))
-
 (define (one-in-three choice)
   (cond
     [(= choice 1) '(1 0 0)]
     [(= choice 2) '(0 1 0)]
     [(= choice 3) '(0 0 1)]
-    [else (error "ERROR: Incorrect choice given for scenario!")]
     ))
-
 (define (gen3) (one-in-three 3))
-
 (define (trials size winc counter)
   (cond
     [(= counter size) winc]
@@ -22,10 +16,9 @@
      (trials size (wins (wins-stay winc) (+ 1(wins-switch winc))) (+ 1 counter))]
     [else (trials size (wins (+ 1 (wins-stay winc)) (wins-switch winc)) (+ 1 counter))]
     ))
-
 (define (Monty-Hall size)
   (trials size (wins 0 0) 0))
-
+;;^Woohoo, twenty lines! (1 struct, five functions)
 ;;==================================================================================================
 
 (Monty-Hall 100000)
